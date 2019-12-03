e1a19dcfbb2321d2a0fecf0476d415bac51b770e
Meg Gutshall <meghan.gutshall@gmail.com>
2019-12-03 01:10:12 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-// Challenge 1
-
 async function loadImages() {
   const frame = document.getElementById('dog-image-container');
   const imageData = await fetchImages();
@@ -20,67 +18,73 @@ async function fetchImages() {
   return await resp.json();
 };
 
-// Challenge 2
+// Render list of breeds
+  // Replace list of breeds according to dropdown filter
+    // Listen for change
+    // Retrieve selected value
+    // Filter breeds by starting letter
+    // Render new breeds list
 
-function fetchBreeds() {
-  fetch("https://dog.ceo/api/breeds/list/all")
-    .then(resp => resp.json())
-    .then(breedData => objectifyBreeds(breedData) )
-};
-
-function objectifyBreeds(breedData) {
-  const breeds = breedData.message;
-  renderBreeds(breeds);
-  filterBreeds(breeds);
-}
-
-function renderBreeds(breeds) {
+async function loadBreeds() {
   const breedList = document.getElementById('dog-breeds');
+  const dropdown = document.getElementById('breed-dropdown');
+  const allBreeds = await fetchBreeds();
+  renderBreeds(allBreeds);
 
-  for (const key in breeds) {
-    const dogBreed = document.createElement('li');
-    dogBreed.innerText = key;
-    dogBreed.addEventListener('click', textColor);
-
-    // Challenge 3
-
-    function textColor(event) {
-      event.currentTarget.style.color = "#009AE4";
+  function emptyBreedList() {
+    const liCollection = document.querySelectorAll('#dog-breeds li');
+    for (let i = 0; li = liCollection[i]; i++) {
+      li.parentNode.removeChild(li);
     };
-    
-    if (Array.isArray(breeds[key]) && breeds[key].length) {
-      const breedSubList = document.createElement('ul');
-      
-      breeds[key].forEach(subBreed => {
-        const dogSubBreed = document.createElement('li');
-        dogSubBreed.innerText = subBreed;
-        dogSubBreed.style.color = "#000000";
-        dogSubBreed.addEventListener('click', textColor);
-        breedSubList.appendChild(dogSubBreed);
-      })
-      
-      dogBreed.appendChild(breedSubList);
-    }
+  };
 
-    breedList.appendChild(dogBreed);
-  }
-};
-
-// Challenge 4
-
-function filterBreeds(breeds) {
-  let dropdown = document.getElementById('breed-dropdown');
   dropdown.addEventListener('change', (event) => {
+    emptyBreedList();
     let letter = event.target.value;
-    const mainBreeds = Object.keys(breeds);
+    const mainBreeds = Object.keys(allBreeds);
     let filtered = mainBreeds.filter(mainBreed => mainBreed.startsWith(letter));
-
+    renderBreeds(filtered);
   });
+
+  function renderBreeds(breeds) {
+    for (const key in breeds) {
+      const dogBreed = document.createElement('li');
+      dogBreed.innerText = key;
+      dogBreed.addEventListener('click', textColor);
+      
+      if (Array.isArray(breeds[key]) && breeds[key].length) {
+        const breedSubList = document.createElement('ul');
+        
+        breeds[key].forEach(subBreed => {
+          const dogSubBreed = document.createElement('li');
+          dogSubBreed.innerText = subBreed;
+          dogSubBreed.style.color = "#000000";
+          dogSubBreed.addEventListener('click', textColor);
+          breedSubList.appendChild(dogSubBreed);
+        })
+        
+        dogBreed.appendChild(breedSubList);
+      }
+  
+      breedList.appendChild(dogBreed);
+    }
+  };
+
+  function textColor(event) {
+    event.currentTarget.style.color = "#009AE4";
+  };
+
+  async function fetchBreeds() {
+    const resp = await fetch("https://dog.ceo/api/breeds/list/all");
+    const data = await resp.json();
+    return data.message;
+  };
 };
+
 
 document.addEventListener('DOMContentLoaded', function() {
   loadImages();
-  fetchBreeds();
+  loadBreeds();
 });
 
 // ** DO WHAT WORKS NOW - REFACTOR LATER!!
