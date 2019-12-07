b04ef381efed14a1ce6303be19b34870d5a98197
Meg Gutshall <meghan.gutshall@gmail.com>
2019-12-07 16:24:35 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
 async function loadImages() {
-  // Make the function asynchronous because the imageLinks array value requires an await
+  // !! Make the function asynchronous because the arrayImageSrcs array value requires an await
   const frame = document.getElementById('dog-image-container');
-  // 
-  const imageLinks = await fetchImages();
-  renderImages(imageLinks);
-  // Pass the result of the fetch into the renderImages function
+  // Find the div element by its ID and save it to a new variable called frame
+  const arrayImageSrcs = await fetchImages();
+  // Assign the result of the fetchImages function to the new arrayImageSrcs variable
+  renderImages(arrayImageSrcs);
+  // Invoke the renderImages function, passing arrayImageSrcs variable as its argument
   
   function renderImages(imageLinks) {
+  // Define the renderImages function with a parameter of imageLinks (which should be an array of image SRCs)
     imageLinks.forEach(imageLink => {
-    // Iterate through the imageLinks array
+    // Iterate through the imageLinks array; for each imageLink...
       const dogImage = document.createElement('img');
-      // Create an image element for each link
+      // Create an image element and save it to a variable called dogImage
       dogImage.src = imageLink;
-      // Set the element's SRC property to the link URL
+      // Then, set dogImage's SRC property to equal the value of imageLink (in order words, the image SRC)
       frame.appendChild(dogImage);
+      // Take dogImage and append it to frame by nesting it as a child element
     })
   };
 };
@@ -28,27 +31,7 @@ async function loadBreeds() {
   const breedList = document.getElementById('dog-breeds');
   const dropdown = document.getElementById('breed-dropdown');
   const allBreeds = await fetchBreeds();
-  renderBreeds(allBreeds);
-
-  function emptyBreedList() {
-    const liCollection = document.querySelectorAll('#dog-breeds li');
-    for (let i = 0; li = liCollection[i]; i++) {
-      li.parentNode.removeChild(li);
-    };
-  };
-
-  dropdown.addEventListener('change', (event) => {
-    emptyBreedList();
-    let letter = event.target.value;
-    const mainBreeds = Object.keys(allBreeds);
-    let filtered = mainBreeds.filter(mainBreed => mainBreed.startsWith(letter));
-    let filteredBreeds = {}
-    for (const element of filtered) {
-      filteredBreeds[element] = [];
-    }
-    renderBreeds(filteredBreeds);
-  });
-
+  
   function renderBreeds(breeds) {
     for (const key in breeds) {
       const dogBreed = document.createElement('li');
@@ -68,15 +51,36 @@ async function loadBreeds() {
         
         dogBreed.appendChild(breedSubList);
       }
-  
+      
       breedList.appendChild(dogBreed);
     }
   };
+
+  function emptyBreedList() {
+    const liCollection = document.querySelectorAll('#dog-breeds li');
+    for (let i = 0; li = liCollection[i]; i++) {
+      li.parentNode.removeChild(li);
+    };
+  };
+
+  dropdown.addEventListener('change', (event) => {
+    emptyBreedList();
+    let letter = event.target.value;
+    const mainBreeds = Object.keys(allBreeds);
+    // Figure something out here if I want to include to the subbreeds
+    let filtered = mainBreeds.filter(mainBreed => mainBreed.startsWith(letter));
+    let filteredBreeds = {}
+    for (const key of filtered) {
+      filteredBreeds[key] = allBreeds[key];
+    }
+    renderBreeds(filteredBreeds);
+  });
 
   function textColor(event) {
     event.currentTarget.style.color = "#009AE4";
   };
 
+  renderBreeds(allBreeds);
 };
 
 async function fetchBreeds() {
